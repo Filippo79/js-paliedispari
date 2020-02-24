e1f36b20df2d018fc6b430d9d26f8e97256515f3
Filippo Pittau <filippo.pittau@gmail.com>
2020-02-24 16:34:36 +0100

    Commit 1

---
+++ js/main.js
@@ -0,0 +1,27 @@
+//Una funzione per capire se la parola è palindroma
+//1 creo un prompt per fare inserire la parola
+//2 creo variabile per la funzione
+//3 creo un if con il log. palidroma o no
+//4 creo la funzione per capire se e palidroma ?? cercare sul web
+
+// 1 creo un prompt per inserire la parola
+var parolaInserita = prompt('Inserisci un numero per stabilire se è palindroma');
+//2 creo variabile per la funzione
+var parolaPalindroma = palindroma(parolaInserita);
+console.log(parolaPalindroma);
+
+//3 creo un if con il log. palidnroma o no
+if (parolaInserita == parolaPanlidroma) {
+    console.log('La parola è palidroma');
+    } else {
+        console.log('La parola non è palidroma');
+    }
+
+//4 creo la funzione per capire se e palidroma ?? cercare sul web
+function palindroma(parola) {
+    var parolaReverse = '';
+    for (var i = parola.length; i > 0; i--) {
+        parolaReverse += parola[i]
+    }
+    return parolaReverse ;
+}
